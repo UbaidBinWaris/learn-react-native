82b07e830dd04fb165f0b2e46d63a1e6d8e92828
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-08-30 09:10:29 -0700

    idk what is happening

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-200">
+      <Text className="text-2xl font-bold text-red-600">
+        Hello Tailwind in React Native 🎉
+      </Text>
+    </View>
+  );
+}
+
