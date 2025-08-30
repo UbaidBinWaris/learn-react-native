b8c8a037df3373326c1530e0565e97c1d67a6c0b
Ubaid Bin Waris <108697112+UbaidBinWaris@users.noreply.github.com>
2025-08-30 09:39:54 -0700

    now start working on design of mobile app

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-blue-200">
-      <Text className="text-2xl font-bold text-red-600">
-        Hello Tailwind in React Native 🎉
-      </Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
